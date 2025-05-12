0f61e6ea01b546fd41cb334edc82b1fe0635bcde
18-King <jainridhim3@gmail.com>
2025-05-12 14:58:49 +0530

    Implemented auth and Storage

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { cert, getApps, initializeApp } from 'firebase-admin/app'
+import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBjYwl56W6_Z8gpHlfVVPNz7mXMz6pIJss",
+  authDomain: "prepwise-851f5.firebaseapp.com",
+  projectId: "prepwise-851f5",
+  storageBucket: "prepwise-851f5.firebasestorage.app",
+  messagingSenderId: "833934511128",
+  appId: "1:833934511128:web:5325893ac2acf5e566876d",
+  measurementId: "G-BBV3V15EMS"
 };
 
 // Initialize Firebase
